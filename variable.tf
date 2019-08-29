0df96ad5e3c284ad449d7016c1ca5fce23927adc
makpal3003 <makpal.aitkazinova@gmail.com>
2019-08-29 17:21:55 -0500

    added variables

---
+++ variable.tf
@@ -0,0 +1,28 @@
+variable "Name" {}
+variable "Env" {}
+variable "Created_by" {}
+variable "Dept" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "user_data" {}
+variable "region" {}
+variable "instance_count" {}
+variable "appname" {}
+variable "min_app_size" {
+}
+variable "max_app_size" {
+  
+}
+variable "desired_app_capacity" {
+  
+}
+variable "min_db_size" {
+  
+}
+variable "max_db_size" {
+  
+}
+variable "desired_db_capacity" {
+  
+}
